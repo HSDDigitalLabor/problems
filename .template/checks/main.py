28c262a1d935c361d7c58df87e9885d0496e4089
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-08-27 12:06:29 +0200

    add .template

---
+++ .template/checks/main.py
@@ -0,0 +1,32 @@
+import check50
+
+FILE_NAME = "willkommen.py"
+
+
+@check50.check()
+def exists():
+    """file exists"""
+    check50.exists(FILE_NAME)
+
+
+@check50.check(exists)
+def testFile():
+    """check output of file"""
+    from re import match
+
+    expected = ""
+    actual = check50.run(f"python3 {FILE_NAME}").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check()
+def no_forbidden_methods():
+    """solution does not use forbidden built-ins"""
+    FORBIDDEN = ["str.lower(", "str.upper(", ".count("]
+    with open("willkommen.py") as f:
+        code = f.read()
+    for method in FORBIDDEN:
+        if method in code:
+            raise check50.Failure(f"Forbidden method used: {method}")
