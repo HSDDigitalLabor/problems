0946186270f04582f66f1d5a05294dd093b9d411
Markus Machmerth <markus.machmerth@hs-duesseldorf.de>
2025-09-09 16:41:07 +0200

    add strings.py

---
+++ strings/checks/main.py
@@ -0,0 +1,19 @@
+import check50
+
+
+@check50.check()
+def exists():
+    """string.py exists"""
+    check50.exists("string.py")
+
+
+@check50.check(exists)
+def testString():
+    """check output of string.py"""
+    from re import match
+
+    expected = "equal"
+    actual = check50.run("python3 string.py").stdin("HellO woRLd!").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(f"{expected}\n", actual, help=help)
