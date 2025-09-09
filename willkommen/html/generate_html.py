67c59b63e4c9667e737ee168ffb5c2111a36ea06
Markus Machmerth <markus.machmerth@hs-duesseldorf.de>
2025-09-09 11:32:43 +0200

    add python file which generate willkommen.html from template

---
+++ willkommen/html/generate_html.py
@@ -0,0 +1,53 @@
+import os
+import datetime
+
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+
+TEMPLATE_NAME = 'willkommen_template.html'
+OUTPUT_FILE_NAME = 'willkommen.html'
+
+render_params = {
+    "id": "willkommen",
+    "title": "Willkommen",
+    "foldername": "willkommen",
+    "filename": "willkommen.py",
+    "exercises": "und implementieren Sie ein Python-Programm, das &quot;Willkommen, Welt!&quot; ausgibt",
+    "details": [
+        {'content': 'Verwenden Sie die Funktion\n'
+                    '<code class="text-danger border border-dark">print()</code>,\n'
+                    'um auf dem Terminal den Text auszugeben. Weitere Informationen finden Sie in der\n'
+                    '<a href="https://docs.python.org/3/library/functions.html#print" target="_blank">\n'
+                    'docs.python.org/3/library/functions.html#print </a>.'}
+    ],
+    "asciicast_id": "zv3diegZOWpOAkMwz2AHYIPd1",
+    "check50_path": "HSDDigitalLabor/problems/git2025/willkommen",
+    "submit50_path": "HSDDigitalLabor/problems/git2025/willkommen",
+    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+}
+
+class HtmlGenerator(object):
+    def __init__(self, template_name):
+        self.template_name = template_name
+        self.env = Environment(loader=FileSystemLoader('../../html_templates'))
+
+    def _build_path(self, suffix):
+        # Build the full file path based on our current directory
+        current_directory = os.getcwd()
+        return os.path.join(current_directory, suffix)
+
+    def generate(self):
+        # Get Jinja template
+        template_path = os.path.join(os.getcwd(), self.template_name)
+        with open(template_path, 'r', encoding='utf-8') as f:
+            template_source = f.read()
+        template = self.env.from_string(template_source)
+        with open(self._build_path(OUTPUT_FILE_NAME), 'w', encoding='utf-8') as html_file:
+            html = template.render(render_params)
+            html_file.write(html)
+
+if __name__ == '__main__':
+    print("Generating HTML...")
+    html_generator = HtmlGenerator(TEMPLATE_NAME)
+    html_generator.generate()
