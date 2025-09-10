1543c33482843bae1e9be091d05d72d79d67a370
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-09-10 13:07:18 +0200

    add additional tests for string.py output validation

--- strings/checks/main.py
+++ strings/checks/main.py
@@ -13,6 +13,18 @@ def testString():
     from re import match
 
     expected = "equal"
+    actual = check50.run("python3 string.py").stdin("HellO woRLd").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(f"{expected}\n", actual, help=help)
+
+
+@check50.check(exists)
+def testInvalidString():
+    """check output of string.py"""
+    from re import match
+
+    expected = "unequal"
     actual = check50.run("python3 string.py").stdin("HellO woRLd!").stdout()
     if not match(expected, actual):
         help = None
