c9c879022c4bd41735004acaa1357afa6e73a686
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-09-10 13:47:21 +0200

    playback: add checks for examples and one additional test

--- playback/checks/main.py
+++ playback/checks/main.py
@@ -11,11 +11,38 @@ def exists():
 
 @check50.check(exists)
 def testFile():
-    """check output of file"""
+    """checking output on first example"""
     from re import match
 
-    expected = ""
-    actual = check50.run(f"python3 {FILE_NAME}").stdout()
+    input = "Grundlagen der Informatik II"
+    expected = "Grundlagen...der...Informatik...II"
+    actual = check50.run(f"python3 {FILE_NAME}").stdin(input).stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(exists)
+def testFile2():
+    """checking output on second example"""
+    from re import match
+
+    input = "Python ist einfach zu lernen und vielseitig einsetzbar."
+    expected = "Python...ist...einfach...zu...lernen...und...vielseitig...einsetzbar."
+    actual = check50.run(f"python3 {FILE_NAME}").stdin(input).stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(exists)
+def testFile3():
+    """checking output on test input"""
+    from re import match
+
+    input = "Test Test"
+    expected = input.replace(" ", "...")
+    actual = check50.run(f"python3 {FILE_NAME}").stdin(input).stdout()
     if not match(expected, actual):
         help = None
         raise check50.Mismatch(expected, actual, help=help)
