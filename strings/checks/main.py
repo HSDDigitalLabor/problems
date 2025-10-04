3417fcb29d0439c625d8b7daba2f1557e28091fe
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-10-04 18:56:52 +0200

    strings: update checks

--- strings/checks/main.py
+++ strings/checks/main.py
@@ -10,22 +10,22 @@ def exists():
 @check50.check(exists)
 def testString():
     """check output of string.py"""
-    from re import match
 
     expected = "equal"
     actual = check50.run("python3 string.py").stdin("HellO woRLd").stdout()
-    if not match(expected, actual):
+    if not actual.startswith(expected):
         help = None
-        raise check50.Mismatch(f"{expected}\n", actual, help=help)
+        msg = f"{expected}\n"
+        raise check50.Mismatch(msg, actual, help=help)
 
 
 @check50.check(exists)
 def testInvalidString():
     """check output of string.py"""
-    from re import match
 
     expected = "unequal"
     actual = check50.run("python3 string.py").stdin("HellO woRLd!").stdout()
-    if not match(expected, actual):
+    if not actual.startswith(expected):
         help = None
-        raise check50.Mismatch(f"{expected}\n", actual, help=help)
+        msg = f"{expected}\n"
+        raise check50.Mismatch(msg, actual, help=help)
