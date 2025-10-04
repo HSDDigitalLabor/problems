43ad4b6f546c1a2a731db99f181b6f3a08d39362
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-10-04 18:55:59 +0200

    faces: add more tests

--- faces/checks/main.py
+++ faces/checks/main.py
@@ -8,12 +8,36 @@ def exists():
 
 
 @check50.check(exists)
-def testFaces():
+def testFaceUp():
     """check output of faces.py"""
-    from re import match
 
     expected = "Hallo 🙂"
     actual = check50.run("python3 faces.py").stdin("Hallo :)").stdout()
-    if not match(expected, actual):
+    if not actual.startswith(expected):
         help = None
-        raise check50.Mismatch(f"{expected}\n", actual, help=help)
+        msg = f"{expected}\n"
+        raise check50.Mismatch(msg, actual, help=help)
+
+
+@check50.check(exists)
+def testFaceDown():
+    """check output of faces.py"""
+
+    expected = "Hallo 🙁"
+    actual = check50.run("python3 faces.py").stdin("Hallo :(").stdout()
+    if not actual.startswith(expected):
+        help = None
+        msg = f"{expected}\n"
+        raise check50.Mismatch(msg, actual, help=help)
+
+
+@check50.check(exists)
+def testFaceTwo():
+    """check output of faces.py"""
+
+    expected = "Hallo 🙂, Tschüss 🙁"
+    actual = check50.run("python3 faces.py").stdin("Hallo :), Tschüss :(").stdout()
+    if not actual.startswith(expected):
+        help = None
+        msg = f"{expected}\n"
+        raise check50.Mismatch(msg, actual, help=help)
