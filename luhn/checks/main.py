0e098264c45b1b3da9cfc0a718176b5232be81db
Markus Machmerth <markus.machmerth@hs-duesseldorf.de>
2025-10-12 09:59:13 +0200

    add checks for Luhn

---
+++ luhn/checks/main.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import check50
+
+FILE_NAME = "luhn.py"
+
+
+@check50.check()
+def exists():
+    """file exists"""
+    check50.exists(FILE_NAME)
+
+
+@check50.check(exists)
+def testFile():
+    """check output of file"""
+    from re import match
+
+    expected = "Die UIC-Wagennummer ist gültig."
+    actual = check50.run(f"python3 {FILE_NAME}").stdin("31 81 665 0 286-0").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(exists)
+def testFile():
+    """check output of file"""
+    from re import match
+
+    expected = "Die UIC-Wagennummer ist gültig."
+    actual = check50.run(f"python3 {FILE_NAME}").stdin("93 81 4 011 090-0").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(exists)
+def testFile():
+    """check output of file"""
+    from re import match
+
+    expected = "Die UIC-Wagennummer ist gültig."
+    actual = check50.run(f"python3 {FILE_NAME}").stdin("93 81 4 011 091-8").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(exists)
+def testFile():
+    """check output of file"""
+    from re import match
+
+    expected = "Die UIC-Wagennummer ist ungültig [Prüfziffer: 0]."
+    actual = check50.run(f"python3 {FILE_NAME}").stdin("93 81 4 011 090-6").stdout()
+    if not match(expected, actual):
+        help = None
+        raise check50.Mismatch(expected, actual, help=help)
