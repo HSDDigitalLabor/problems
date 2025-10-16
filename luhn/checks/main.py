7b362e992398c0a7d280e86076cbf7ab87bdc8c7
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-10-16 15:41:37 +0200

    luhn: fix testUIC4()

--- luhn/checks/main.py
+++ luhn/checks/main.py
@@ -50,9 +50,9 @@ def testUIC3():
 @check50.check(exists)
 def testUIC4():
     """check invalid UIC: 93 81 4 011 090-6"""
-    from re import match
+    from re import escape, match
 
-    expected = "Die UIC-Wagennummer ist ungültig [Prüfziffer: 0].\n"
+    expected = escape("Die UIC-Wagennummer ist ungültig [Prüfziffer: 0].\n")  # escape
     actual = check50.run(f"python3 {FILE_NAME}").stdin("93 81 4 011 090-6").stdout()
     if not match(expected, actual):
         help = None
