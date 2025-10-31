1dc2c8f66d2946a0dcb3f65d0e6b0d209486ddd0
Leon K. <101114069+LeoKle@users.noreply.github.com>
2025-10-31 10:58:01 +0100

    emojize: disallow str.replace(), add check for emoji.emojize() (#42)

--- emojize/checks/main.py
+++ emojize/checks/main.py
@@ -1,3 +1,6 @@
+import io
+import re
+import tokenize
 from pathlib import Path
 
 import check50
@@ -19,6 +22,35 @@ def uses_package():
         if "import emoji" not in source and "from emoji" not in source:
             msg = "The emoji package must be used."
             raise check50.Failure(msg)
+
+
+@check50.check(uses_package)
+def no_str_replace():
+    """solution must not use str.replace()"""
+    source = Path(FILE_NAME).read_text()
+    tokens = tokenize.generate_tokens(io.StringIO(source).readline)
+    for token_type, token_string, *_ in tokens:
+        # only check actual code (not comments, not strings)
+        if token_type == tokenize.NAME and token_string == "replace":
+            msg = "The use of str.replace() is not allowed."
+            raise check50.Failure(msg)
+
+
+@check50.check(uses_package)
+def uses_emoji_emojize():
+    """solution must call emoji.emojize()"""
+    source = Path(FILE_NAME).read_text()
+
+    # remove comments before checking
+    io_obj = io.StringIO(source)
+    tokens = tokenize.generate_tokens(io_obj.readline)
+    code_no_comments = "".join(
+        tokval for toktype, tokval, *_ in tokens if toktype != tokenize.COMMENT
+    )
+
+    if "emoji.emojize(" not in code_no_comments.replace(" ", ""):
+        msg = "Your solution must call emoji.emojize()."
+        raise check50.Failure(msg)
 
 
 @check50.check(uses_package)
